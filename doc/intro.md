e8c9c5deb8b8247d9b59e3c72f8c1dfeb766b61a
qespr <rspointer@post.cz>
2023-01-01 00:36:36 +0100

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to matrixies
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/matrixies/core_test.clj
@@ -0,0 +1,7 @@
+(ns matrixies.core-test
+  (:require [clojure.test :refer :all]
+            [matrixies.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
