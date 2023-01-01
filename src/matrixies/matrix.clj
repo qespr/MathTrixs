2aebbf6656325d2d0ff65752b57683ee45b0aede
qespr <rspointer@post.cz>
2023-01-01 13:44:21 +0100

    Added matrix validation function

---
+++ src/matrixies/matrix.clj
@@ -0,0 +1,18 @@
+(ns matrixies.matrix
+  (:gen-class))
+
+(defn valid?
+  "Checks whether given matrix is valid"
+  [m]
+  ;;Checks m only contains vectors and
+  ;;these vectors only contain numbers
+  ;;All inner vectors must have the same length
+  (and
+   (vector? m)
+   (< 0 (count m))
+   (every? vector? m)
+   (let [len (count (first m))]
+     (every? (fn [x]
+             (and (= len (count x))
+                  (every? number? x))) m))))
+
